cf9394869076705a66ba2af270c6c083cc457050
emmanueldav <kuradavis987@gmail.com>
2024-08-26 00:05:24 +0100

    fixed issues on progress screen

--- app/src/main/java/com/cyberiyke/fitfuel/ui/composables/components/UiComponents.kt
+++ app/src/main/java/com/cyberiyke/fitfuel/ui/composables/components/UiComponents.kt
@@ -1,13 +1,24 @@
 package com.cyberiyke.fitfuel.ui.composables.components
 
 import android.net.Uri
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.cyberiyke.fitfuel.R
 
@@ -32,6 +43,39 @@ fun UserProfilePix(
         contentScale = ContentScale.Crop,
         colorFilter = if (imgUri == null) ColorFilter.tint(color = tint) else null
     )
+}
 
 
+@Composable
+fun RunningStats(
+    modifier: Modifier = Modifier, painter: Painter, unit: String, value: String
+) {
+
+
+    Row(modifier = Modifier.padding(4.dp)) {
+        Image(
+            painter = painter,
+            contentDescription = null,
+            modifier = Modifier
+                .padding(top = 5.dp)
+                .size(20.dp)
+        )
+
+        Spacer(modifier = Modifier.size(12.dp))
+
+        Column(modifier = Modifier.padding(top = 8.dp), horizontalAlignment = Alignment.End) {
+            androidx.compose.material3.Text(
+                text = value,
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.SemiBold),
+            )
+            androidx.compose.material3.Text(
+                text = unit,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                style = MaterialTheme.typography.labelSmall
+            )
+
+        }
+
+    }
 }
--- app/src/main/java/com/cyberiyke/fitfuel/ui/composables/tabs/profile/ProfileScreen.kt
+++ app/src/main/java/com/cyberiyke/fitfuel/ui/composables/tabs/profile/ProfileScreen.kt
@@ -1,13 +1,41 @@
 package com.cyberiyke.fitfuel.ui.composables.tabs.profile
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cyberiyke.fitfuel.R
+import com.cyberiyke.fitfuel.ui.composables.components.RunningStats
 
 /**
  * Composable function that represents the home screen of the application.
@@ -16,9 +44,136 @@ import androidx.compose.ui.graphics.Color
 fun ProfileScreen() {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Text(
-            text = "Search",
-            style = typography.titleLarge,
-            color = Color.Cyan
+            text = "Search", style = typography.titleLarge, color = Color.Cyan
         )
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun SettingsItem(
+    modifier: Modifier = Modifier,
+//    img: Painter,
+//    title: String,
+    showDivider: Boolean = true
+) {
+
+    ElevatedCard(shape = RoundedCornerShape(5.dp), modifier = Modifier.padding(10.dp)) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 24.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.baseline_male_24),
+                contentDescription = "title",
+                modifier = Modifier.size(32.dp),
+            )
+            Spacer(modifier = Modifier.size(16.dp))
+            Text(
+                text = "title",
+                style = typography.titleMedium.copy(
+                    color = MaterialTheme.colorScheme.onSurface, fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier.weight(1f),
+            )
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.baseline_arrow_forward_ios_24),
+                contentDescription = null,
+                modifier = Modifier.size(16.dp),
+                tint = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.4f)
+            )
+        }
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun TotalProgressCard(modifier: Modifier = Modifier) {
+
+    ElevatedCard(
+        modifier = Modifier, elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+                .fillMaxWidth()
+        ) {
+
+            Text(
+                text = "Total Progress", style = typography.labelLarge.copy(
+                    color = MaterialTheme.colorScheme.onSurface, fontWeight = FontWeight.Bold
+                ), modifier = Modifier.weight(1f)
+            )
+
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.baseline_arrow_forward_ios_24),
+                contentDescription = "More Info",
+                modifier = Modifier
+                    .size(16.dp)
+                    .align(Alignment.CenterVertically),
+                tint = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 16.dp)
+                .fillMaxWidth()
+                .border(
+                    width = 2.dp,
+                    color = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f),
+                    shape = MaterialTheme.shapes.small
+                ).padding(8.dp), horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            RunningStats(
+                painter = painterResource(id = R.drawable.running_man),
+                unit = "10",
+                value = "km",
+                modifier = Modifier
+            )
+            Box(
+                modifier = Modifier
+                    .width(1.dp)
+                    .fillMaxHeight()
+                    .padding(vertical = 8.dp)
+                    .align(Alignment.CenterVertically)
+                    .background(
+                        color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.5f),
+                    )
+            )
+            RunningStats(
+                modifier = Modifier,
+                painter = painterResource(id = R.drawable.stopwatch),
+                unit = "hr",
+                value = "20Km"
+            )
+            Box(
+                modifier = Modifier
+                    .width(1.dp)
+                    .fillMaxHeight()
+                    .padding(vertical = 8.dp)
+                    .align(Alignment.CenterVertically)
+                    .background(
+                        color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.5f),
+                    )
+            )
+            RunningStats(
+                modifier = Modifier,
+                painter = painterResource(id = R.drawable.fire),
+                unit = "kcal",
+                value = "10"
+            )
+
+        }
+    }
+
+
 }
